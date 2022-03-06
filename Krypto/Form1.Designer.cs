0cd17cf03aabcd0f66793d3d508dfcd1dcfb8930
Bartosz Drągowski <236521@edu.p.lodz.pl>
2022-03-06 21:16:07 +0100

    First GUI created.

--- Krypto/Form1.Designer.cs
+++ Krypto/Form1.Designer.cs
@@ -28,13 +28,151 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.generujButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.szyfrujButton = new System.Windows.Forms.Button();
+            this.deszyfrujButton = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.keyLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // generujButton
+            // 
+            this.generujButton.Location = new System.Drawing.Point(421, 104);
+            this.generujButton.Name = "generujButton";
+            this.generujButton.Size = new System.Drawing.Size(117, 23);
+            this.generujButton.TabIndex = 0;
+            this.generujButton.Text = "Generuj Klucz";
+            this.generujButton.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(47, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 57);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(190, 265);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(222, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label2";
+            // 
+            // szyfrujButton
+            // 
+            this.szyfrujButton.Location = new System.Drawing.Point(421, 157);
+            this.szyfrujButton.Name = "szyfrujButton";
+            this.szyfrujButton.Size = new System.Drawing.Size(117, 23);
+            this.szyfrujButton.TabIndex = 5;
+            this.szyfrujButton.Text = "Szyfruj";
+            this.szyfrujButton.UseVisualStyleBackColor = true;
+            // 
+            // deszyfrujButton
+            // 
+            this.deszyfrujButton.Location = new System.Drawing.Point(421, 205);
+            this.deszyfrujButton.Name = "deszyfrujButton";
+            this.deszyfrujButton.Size = new System.Drawing.Size(117, 23);
+            this.deszyfrujButton.TabIndex = 6;
+            this.deszyfrujButton.Text = "Deszyfruj";
+            this.deszyfrujButton.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(541, 19);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 16);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "label3";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(225, 57);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(190, 265);
+            this.textBox4.TabIndex = 9;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(544, 57);
+            this.textBox5.Multiline = true;
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(190, 265);
+            this.textBox5.TabIndex = 10;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(418, 34);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(94, 16);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Aktualny klucz:";
+            // 
+            // keyLabel
+            // 
+            this.keyLabel.AutoSize = true;
+            this.keyLabel.Location = new System.Drawing.Point(427, 65);
+            this.keyLabel.Name = "keyLabel";
+            this.keyLabel.Size = new System.Drawing.Size(40, 16);
+            this.keyLabel.TabIndex = 12;
+            this.keyLabel.Text = "lalala";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(750, 340);
+            this.Controls.Add(this.keyLabel);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.deszyfrujButton);
+            this.Controls.Add(this.szyfrujButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.generujButton);
+            this.Name = "Form1";
+            this.Text = "Krypto";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button generujButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button szyfrujButton;
+        private System.Windows.Forms.Button deszyfrujButton;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label keyLabel;
     }
 }
 
