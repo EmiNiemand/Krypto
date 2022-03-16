faff2bfdb083f3788cafa7baceba4f2c12813e81
EmiNiemand <Szymon_PL@live.com>
2022-03-16 13:18:34 +0100

    Ver. 0.0.1 Pre Alpha

---
+++ Krypto/DES.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Krypto
+{
+    public class DES
+    {
+        int[] key = new int[64];
+        byte[] binaryText;
+        
+        byte[] ReadFile(string path)
+        {
+            byte[] bytes = File.ReadAllBytes(path);
+            return bytes;
+        }
+        
+        byte[] StrToBin(string text)
+        {
+            Encoding encoding = Encoding.UTF8;
+            byte[] bytes = encoding.GetBytes(text);
+            return bytes;
+        }
+        
+    }
+
+}
