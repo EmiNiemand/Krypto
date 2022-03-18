65cf8d24e240ccafa4d8b567fda4d5363e1ff954
EmiNiemand <Szymon_PL@live.com>
2022-03-18 14:15:02 +0100

    Final

--- Krypto/Program.cs
+++ Krypto/Program.cs
@@ -16,20 +16,6 @@ namespace Krypto
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
 
-            DES des = new DES();
-
-            string txt = "dupa";
-            string key = "abcdefgh";
-
-            byte[] crypto = des.Cipher2(txt, key);
-
-            string scrypto = des.BytesToString(crypto);
-
-            string final = des.Decipher2(crypto, key);
-
-            string crypt = des.Cipher(txt, key);
-            string decrypt = des.Decipher(crypt, key);
-
         }
     }
 }
